44c3c002d5f26448756681bb405f5b89126aea7a
Raul Correia <raulvinicius245@gmail.com>
2022-07-25 12:34:21 -0300

    Comunicando com a Poke Api

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get apiListAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap(res => res),
+      tap(res => {
+        console.log(res);
+      })
+    )
+  }
 }
