cdf712981109aceeaa9eac989cf823b0acb6a22d
Raul Correia <raulvinicius245@gmail.com>
2022-07-27 10:06:41 -0300

    Adicionando ao objeto do Pokemon os seus status através de um get no endpoint de cada pokemon da lista

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -18,8 +18,18 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap(res => res),
       tap(res => {
-        console.log(res);
+        res.results.map( (resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+        })
       })
+    )
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(res => res)
     )
   }
 }
