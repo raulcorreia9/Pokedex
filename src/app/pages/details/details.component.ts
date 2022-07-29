e208510249b3e025e59eb5d1780d7205251de779
Raul Correia <raulvinicius245@gmail.com>
2022-07-29 11:02:40 -0300

    Html e Scss para Details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,23 +15,25 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlSpecie: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
 
   }
 
-  get pokemon() {
+  get getPokemon() {
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlSpecie}/${id}`);
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
       }
     )
   }
